2137fa2ac71aa56b1194e42480312d7045294108
Alberto Casas Ortiz <albertocasasortiz@gmail.com>
2020-10-22 12:02:56 -0600

    Implemented ConfigWfas.

--- wfes-lib/source/model/executables/wfas/config/configWfas.cpp
+++ wfes-lib/source/model/executables/wfas/config/configWfas.cpp
@@ -1,6 +1,79 @@
 #include "configWfas.h"
 
-ConfigWfas::ConfigWfas()
-{
+using namespace wfes::config;
 
-}
+ConfigWfas::ConfigWfas() {}
+
+ivec ConfigWfas::N = [] {
+    ivec tmp(1);
+    tmp << 100;
+    return tmp;
+}();
+
+ivec ConfigWfas::G = [] {
+    ivec tmp(1);
+    tmp << 100;
+    return tmp;
+}();
+
+dvec ConfigWfas::f = [] {
+    dvec tmp(1);
+    tmp << 100;
+    return tmp;
+}();
+
+dvec ConfigWfas::u = [] {
+    dvec tmp(1);
+    tmp << 1e-9;
+    return tmp;
+}();
+
+dvec ConfigWfas::v = [] {
+    dvec tmp(1);
+    tmp << 1e-9;
+    return tmp;
+}();
+
+dvec ConfigWfas::s = [] {
+    dvec tmp(1);
+    tmp << 0;
+    return tmp;
+}();
+
+dvec ConfigWfas::h = [] {
+    dvec tmp(1);
+    tmp << 0.5;
+    return tmp;
+}();
+
+std::string ConfigWfas::library = "Pardiso";
+std::string ConfigWfas::vienna_solver = "BicGStab";
+
+double ConfigWfas::a = 1e-20;
+int ConfigWfas::p = 1;
+int ConfigWfas::n_threads = 1;
+bool ConfigWfas::no_proj = false;
+
+bool ConfigWfas::output_Q = false;
+bool ConfigWfas::output_R = false;
+bool ConfigWfas::output_N = false;
+bool ConfigWfas::output_B = false;
+bool ConfigWfas::output_N_Ext = false;
+bool ConfigWfas::output_N_Fix = false;
+bool ConfigWfas::output_N_Tmo = false;
+bool ConfigWfas::output_Dist = true;
+
+std::string ConfigWfas::initial_distribution_csv = "";
+std::string ConfigWfas::path_output_Q = "output_Q.csv";
+std::string ConfigWfas::path_output_R = "output_R.csv";
+std::string ConfigWfas::path_output_N = "output_N.csv";
+std::string ConfigWfas::path_output_B = "output_B.csv";
+std::string ConfigWfas::path_output_N_Ext = "output_N_Ext.csv";
+std::string ConfigWfas::path_output_N_Fix = "output_N_Fix.csv";
+std::string ConfigWfas::path_output_N_Tmo = "output_N_Tmo.csv";
+std::string ConfigWfas::path_output_Dist = "output_Dist.csv";
+
+bool ConfigWfas::force = false;
+bool ConfigWfas::verbose = false;
+std::string ConfigWfas::error = "";
+int ConfigWfas::num_comp = 1;
