0a07f2063943a37a1d366f11faa37a73f9705539
Alberto Casas Ortiz <albertocasasortiz@gmail.com>
2020-10-22 11:46:04 -0600

    Implemented ResultsWfas.

--- wfes-lib/source/model/executables/wfas/results/resultsWfas.h
+++ wfes-lib/source/model/executables/wfas/results/resultsWfas.h
@@ -2,10 +2,22 @@
 #define RESULTSWFAS_H
 
 
-class ResultsWfas
-{
-public:
-    ResultsWfas();
+class ResultsWfas {
+    public:
+        double time;
+        dvec probs;
+        ResultsWfas();
+
+        ResultsWfas(double time);
+
+        ResultsWfas(dvec probs, double time);
+
+        /**
+         * @brief Write results in a CSV file.
+         * @param results results to be printed in a CSV file.
+         * @param name Name of the file.
+         */
+        void writeResultsToFile(ResultsWfas *results, std::string name);
 };
 
 #endif // RESULTSWFAS_H
