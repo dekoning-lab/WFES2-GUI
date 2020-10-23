a0146762c609e13db09ec5ecb52e4ffd5b2b6c38
Alberto Casas Ortiz <albertocasasortiz@gmail.com>
2020-10-23 15:02:15 -0600

    Implemented Config for wfes_sequential.

--- wfes-lib/source/model/executables/wfes_sequential/config/configWfesSequential.cpp
+++ wfes-lib/source/model/executables/wfes_sequential/config/configWfesSequential.cpp
@@ -1,6 +1,81 @@
 #include "configWfesSequential.h"
 
-ConfigWfesSequential::ConfigWfesSequential()
-{
+using namespace wfes::config;
 
-}
+ConfigWfesSequential::ConfigWfesSequential() {}
+
+lvec ConfigWfesSequential::N = [] {
+    lvec tmp(1);
+    tmp << 100;
+    return tmp;
+}();
+
+dvec ConfigWfesSequential::t = [] {
+    dvec tmp(1);
+    tmp << 100;
+    return tmp;
+}();
+
+dvec ConfigWfesSequential::p = [] {
+    dvec tmp(1);
+    tmp << 100;
+    return tmp;
+}();
+
+dvec ConfigWfesSequential::u = [] {
+    dvec tmp(1);
+    tmp << 1e-9;
+    return tmp;
+}();
+
+dvec ConfigWfesSequential::v = [] {
+    dvec tmp(1);
+    tmp << 1e-9;
+    return tmp;
+}();
+
+dvec ConfigWfesSequential::s = [] {
+    dvec tmp(1);
+    tmp << 0;
+    return tmp;
+}();
+
+dvec ConfigWfesSequential::h = [] {
+    dvec tmp(1);
+    tmp << 0.6;
+    return tmp;
+}();
+
+std::string ConfigWfesSequential::library = "Pardiso";
+std::string ConfigWfesSequential::vienna_solver = "BicGStab";
+
+double ConfigWfesSequential::a = 1e-20;
+double ConfigWfesSequential::c = 1;
+int ConfigWfesSequential::n_threads = 1;
+
+
+bool ConfigWfesSequential::output_Q = false;
+bool ConfigWfesSequential::output_R = false;
+bool ConfigWfesSequential::output_N = false;
+bool ConfigWfesSequential::output_B = false;
+bool ConfigWfesSequential::output_I = false;
+bool ConfigWfesSequential::output_N_Fix = false;
+bool ConfigWfesSequential::output_N_Tmo = false;
+bool ConfigWfesSequential::output_Res = true;
+
+std::string ConfigWfesSequential::initial_distribution_csv = "";
+std::string ConfigWfesSequential::path_output_Q = "output_Q.csv";
+std::string ConfigWfesSequential::path_output_R = "output_R.csv";
+std::string ConfigWfesSequential::path_output_N = "output_N.csv";
+std::string ConfigWfesSequential::path_output_B = "output_B.csv";
+std::string ConfigWfesSequential::path_output_I = "output_I.csv";
+std::string ConfigWfesSequential::path_output_N_Fix = "output_N_Fix.csv";
+std::string ConfigWfesSequential::path_output_N_Tmo = "output_N_Tmo.csv";
+std::string ConfigWfesSequential::path_output_Res = "output_Res.csv";
+
+bool ConfigWfesSequential::force = false;
+bool ConfigWfesSequential::verbose = false;
+std::string ConfigWfesSequential::error = "";
+int ConfigWfesSequential::num_comp = 1;
+
+
--- wfes-lib/source/model/executables/wfes_sequential/config/configWfesSequential.h
+++ wfes-lib/source/model/executables/wfes_sequential/config/configWfesSequential.h
@@ -1,11 +1,160 @@
 #ifndef CONFIGWFESSEQUENTIAL_H
 #define CONFIGWFESSEQUENTIAL_H
 
+#include "utils/types.h"
 
-class ConfigWfesSequential
-{
-public:
-    ConfigWfesSequential();
-};
+namespace wfes {
+    namespace config {
+
+        class ConfigWfesSequential {
+            public:
+                /**
+                 * @brief Default constructor for class ConfigWfesSequential.
+                 */
+                ConfigWfesSequential();
+
+                /**
+                 * @brief Library used for solving matrix systems (e.g. pardiso, viennacl...).
+                 */
+                static std::string library;
+
+                /**
+                 * @brief Solver used in viennacl. (e.g. biCGStab, GMRes...).
+                 */
+                static std::string vienna_solver;
+
+                static lvec N;
+
+                static lvec t;
+
+                static dvec p;
+
+                static dvec s;
+
+                static dvec h;
+
+                static dvec u;
+
+                static dvec v;
+
+                static double a;
+
+                static double c;
+
+                static int n_threads;
+
+                /**
+                 * @brief Path to initial probability distribution CSV (I).
+                 */
+                static std::string initial_distribution_csv;
+
+                /**
+                 * @brief Output Q.
+                 */
+                static bool output_Q;
+
+                /**
+                 * @brief Output R.
+                 */
+                static bool output_R;
+
+                /**
+                 * @brief Output N.
+                 */
+                static bool output_N;
+
+                /**
+                 * @brief Output B.
+                 */
+                static bool output_B;
+
+                /**
+                 * @brief Output I.
+                 */
+                static bool output_I;
+
+                /**
+                 * @brief Path output N_fix (fixation-conditional sojourn).
+                 */
+                static bool output_N_Fix;
+
+                /**
+                 * @brief Path output N_tmo (timeout-conditional sojourn).
+                 */
+                static bool output_N_Tmo;
+
+                /**
+                 * @brief Output Res (Generated results).
+                 */
+                static bool output_Res;
+
+
+
+                /**
+                 * @brief Path output Q.
+                 */
+                static std::string path_output_Q;
+
+                /**
+                 * @brief Path output R.
+                 */
+                static std::string path_output_R;
+
+                /**
+                 * @brief Path output N.
+                 */
+                static std::string path_output_N;
+
+                /**
+                 * @brief Path output B.
+                 */
+                static std::string path_output_B;
+
+                /**
+                 * @brief Path output I.
+                 */
+                static std::string path_output_I;
+
+                /**
+                 * @brief Path output N_fix (fixation-conditional sojourn).
+                 */
+                static std::string path_output_N_Fix;
+
+                /**
+                 * @brief Path output N_tmo (timeout-conditional sojourn).
+                 */
+                static std::string path_output_N_Tmo;
+
+                /**
+                 * @brief Path output Res (Generated results).
+                 */
+                static std::string path_output_Res;
+
+
+
+                /**
+                 * @brief Do not perform parameter checks.
+                 */
+                static bool force;
+
+                /**
+                 * @brief Verbose solver output (TODO Not Used In GUI.)
+                 */
+                static bool verbose;
+
+                /**
+                 * @brief Error messages to show if there is an error.
+                 */
+                static std::string error;
+
+                /**
+                 * @brief Number of components used.
+                 */
+                static int num_comp;
+
+
+        };
+    }
+}
 
 #endif // CONFIGWFESSEQUENTIAL_H
