edb46ba5a3efa47e6594192fefa41b77f44c5626
Alberto Casas Ortiz <albertocasasortiz@gmail.com>
2020-10-22 18:20:37 -0600

    Implemented workerThread for wfesSequential.

--- wfes-lib/source/model/executables/wfes_sequential/thread/workerThreadWfesSequential.cpp
+++ wfes-lib/source/model/executables/wfes_sequential/thread/workerThreadWfesSequential.cpp
@@ -1,6 +1,29 @@
 #include "workerThreadWfesSequential.h"
 
-WorkerThreadWfesSequential::WorkerThreadWfesSequential()
-{
+WorkerThreadWfesSequential::WorkerThreadWfesSequential(QObject *parent) : QThread(parent) {
+    results = ResultsWfesSequential();
+}
 
+WorkerThreadWfesSequential::~WorkerThreadWfesSequential(){
+    if (!done) {
+        exit();
+        emit updateProgress(wfes::utils::ExecutionStatus::ABORTED);
+    } else {
+        exit();
+    }
+}
+
+void WorkerThreadWfesSequential::run() {
+    QString result;
+    wfes_sequential sequential = wfes_sequential();
+    wfes_sequential.addObserver(this);
+    results = *wfes_sequential.execute();
+
+    done = true;
+    emit resultReady(results);
+}
+
+void WorkerThreadWfesSequential::update(int value)
+{
+    emit updateProgress(value);
 }
--- wfes-lib/source/model/executables/wfes_sequential/thread/workerThreadWfesSequential.h
+++ wfes-lib/source/model/executables/wfes_sequential/thread/workerThreadWfesSequential.h
@@ -1,6 +1,23 @@
 #ifndef WORKERTHREADWFESSEQUENTIAL_H
 #define WORKERTHREADWFESSEQUENTIAL_H
 
+#include <QObject>
+#include <QDebug>
+#include <wfes-lib_global.h>
+
+
+#include <QString>
+#include <QThread>
+
+#include <cmath>
+#include <boost/format.hpp>
+#include <utils/observer/observer.h>
+#include "utils/utils.h"
+#include <source/model/executables/wfes_sequential/results/resultsWfesSequential.h>
+#include <model/executables/wfes_sequential/wfes_sequential.h>
+
+#include "source/model/executables/wfes_sequential/config/configWfesSequential.h"
+
 
 class WorkerThreadWfesSequential
 {
